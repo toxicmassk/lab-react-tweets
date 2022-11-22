853bf9701e10253894934bfcf994bb62da387fb4
Julia Lipniewicz <julialipniewicz@yahoo.de>
2022-11-22 17:19:24 +0100

    done

---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+function Message(props) {
+  return (
+    <div className="tweet">
+      <p className="message">{props.message}</p>
+    </div>
+  );
+}
+
+export default Message;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,9 @@
+function TimeStamp(props) {
+  return (
+    <div className="tweet">
+      <span className="timestamp">{props.time}</span>
+    </div>
+  );
+}
+
+export default TimeStamp;
